0e6b339802e3364938b245f5552042b7d324bbab
Kanstantsin <Kanstantsin_Makarau@epam.com>
2014-09-30 02:01:15 +0300

    Transfer to hostel and some work

--- src/main/java/com/epam/tariff/entity/Tariff.java
+++ src/main/java/com/epam/tariff/entity/Tariff.java
@@ -5,6 +5,39 @@ package com.epam.tariff.entity;
  */
 public class Tariff {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOperatorName() {
+        return operatorName;
+    }
+
+    public void setOperatorName(String operatorName) {
+        this.operatorName = operatorName;
+    }
+
+    public double getPayroll() {
+        return payroll;
+    }
+
+    public void setPayroll(double payroll) {
+        this.payroll = payroll;
+    }
+
+    public double getSmsPrice() {
+        return smsPrice;
+    }
+
+    public void setSmsPrice(double smsPrice) {
+        this.smsPrice = smsPrice;
+    }
+
     private String operatorName;
     private double payroll;
     private double smsPrice;
--- src/main/java/com/epam/tariff/logic/TariffXMLParser.java
+++ src/main/java/com/epam/tariff/logic/TariffXMLParser.java
@@ -1,23 +1,59 @@
 package com.epam.tariff.logic;
 
 import com.epam.tariff.entity.Tariff;
+import com.sun.org.apache.xerces.internal.parsers.DOMParser;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TariffXMLParser {
     private ArrayList<Tariff> tariffs = new ArrayList<Tariff>();
-    private String xmlFileName;
 
     public TariffXMLParser(String xmlFileName) {
-        this.xmlFileName = xmlFileName;
-        parse();
+        try{
+            DOMParser domParser = new DOMParser();
+            domParser.parse(xmlFileName);
+            Document document = domParser.getDocument();
+            Element root = document.getDocumentElement();
+            analyze(root);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public ArrayList<Tariff> getData() {
-        return tariffs;
+        return (ArrayList<Tariff>) Collections.unmodifiableList(tariffs);
     }
 
-    public void parse() {
+    public void analyze(Element root) {
+        NodeList tariffList = root.getElementsByTagName("tariff");
+        Tariff tariff = null;
+        for(int i = 0; i < tariffList.getLength(); i++){
+            tariff = new Tariff();
+            Element tariffElement = (Element) tariffList.item(i);
+            tariff.setName(getBabyValue(tariffElement, "name"));
+            tariff.setOperatorName(getBabyValue(tariffElement, "OperatorName"));
+        }
+    }
 
+    private String getBabyValue(Element parent, String childName) {
+        Element child = getBaby(parent, childName);
+        Node node = child.getFirstChild();
+        String value = node.getNodeValue();
+        return value;
+    }
+
+    private Element getBaby(Element parent, String childName) {
+        NodeList nodeList = parent.getElementsByTagName("name");
+        Element child  = (Element) nodeList.item(0);
+        return child;
     }
 }
